aef85b8d0a4e3040dbdb4463fe1dc2ead6174b23
Douglas Frisk <douglasfrisk@gmail.com>
2011-12-01 03:38:08 +0100

    Added original comments, ran it through JSLint and minified it

--- jquery.slides.js
+++ jquery.slides.js
@@ -1,28 +1,41 @@
-/*!
- * jQuery lightweight plugin boilerplate
- * Original author: @ajpiano
- * Further changes, comments: @addyosmani
- * Licensed under the MIT license
- * 
- * Plugin author: Douglas Frisk
+/*
+ *  Project: jquery-flickrslides
+ *  Description: jQuery tabs plugin
+ *  Author: Douglas Frisk
+ *  License: -
  */
 
 
-;(function ( $, window, document, undefined ) {
-    
-    var pluginName = 'flickrSlides',
-        defaults = {
-            
-        };
+// the semi-colon before function invocation is a safety net against concatenated 
+// scripts and/or other plugins which may not be closed properly.
+;(function ($, window, document, undefined) {
 
-    function FlickrSlidesPlugin( element, options ) {
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+
+    // window and document are passed through as local variables rather than globals
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+    var pluginName = 'flickrSlides',
+        defaults = {};
+
+    // The actual plugin constructor
+    function FlickrSlidesPlugin(element, options) {
         this.element = element;
 
-        this.options = $.extend( {}, defaults, options) ;
-        
+        // jQuery has an extend method which merges the contents of two or 
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend({}, defaults, options);
+
         this._defaults = defaults;
         this._name = pluginName;
-        
+
         this.init();
     }
 
@@ -32,9 +45,12 @@
             photosetId = this.options.photoset_id,
             _this = this.element,
             animationSpeed = 300,
-            rarr = $("<div>").css({"right": 0, "text-align": "right"}).text(")"), // Next photo arrow
-            larr = $("<div>").css({"left": 0, "text-align": "left"}).text("("), // Previous photo arrow
-            presentationDiv = $("<div>").hide(). // The div that holds the photos
+            // Next photo arrow
+            rarr = $("<div>").css({"right": 0, "text-align": "right"}).text(">"),
+            // Previous photo arrow
+            larr = $("<div>").css({"left": 0, "text-align": "left"}).text("<"),
+            // The div that holds the photos
+            presentationDiv = $("<div>").hide().
                 css({
                     "width": "200px",
                     "height": "200px",
@@ -48,13 +64,15 @@
                     "text-align": "center",
                     "background": "#FFF"
                 }),
-            loading = $("<span>"). // Loading spinner
+            // Loading spinner
+            loading = $("<span>").
                 css({
                     "font-family": "'WebSymbolsRegular', Arial, sans-serif",
                     "font-size": "50px",
                     "line-height": "200px"
                 }),
-            bg = $("<div>"). // Faded background
+            // Faded background
+            bg = $("<div>").
                 css({
                     "width": "100%",
                     "height": "100%",
@@ -64,13 +82,21 @@
                     "background": "rgba(0,0,0,0.4)"
                 }).
                 hide().
-                click(function() {
+                click(function () {
                     presentationDiv.hide();
                     bg.hide();
-                });
-                
-                
-        $.map([larr,rarr], function(n) {
+                }),
+            loaderSymbols = ['0', '1', '2', '3', '4', '5', '6', '7'],
+            loaderRate = 100,
+            loaderIndex = 0,
+            loader = function () {
+                loading.text(loaderSymbols[loaderIndex]);
+                loaderIndex = loaderIndex  < loaderSymbols.length - 1 ? loaderIndex + 1 : 0;
+                setTimeout(loader, loaderRate);
+            };
+
+
+        $.map([larr, rarr], function (n) {
             return n.css({
                 "cursor": "pointer",
                 "font-family": "'WebSymbolsRegular', Arial, sans-serif",
@@ -85,99 +111,93 @@
                 "text-shadow": "2px 2px 5px #000",
                 "opacity": 0,
                 "padding": "10px"
-            }).hover(function() {
-                $(this).animate({"opacity":1},0);
-            },function() {
-                $(this).animate({"opacity":0},0);
+            }).hover(function () {
+                $(this).animate({"opacity": 1}, 0);
+            }, function () {
+                $(this).animate({"opacity": 0}, 0);
             });
         });
-        
-        // For the spinner
-        var loaderSymbols = ['0', '1', '2', '3', '4', '5', '6', '7'], 
-            loaderRate = 100, 
-            loaderIndex = 0, 
-            loader= function() { 
-                loading.text(loaderSymbols[loaderIndex]); 
-                loaderIndex = loaderIndex  < loaderSymbols.length - 1 ? loaderIndex + 1 : 0; 
-                setTimeout(loader, loaderRate); 
-            }; 
-        loader(); 
-        
-        
+
+
+        loader();
+
         // Clear "no javascript" text
         $(_this).empty();
-        
-        jsonCallback = function(data) {
+
+        jsonCallback = function (data) {
             var thumbs_ul;
-            
+
             thumbs_ul = $("<ul>");
             $(_this).append(thumbs_ul);
             presentationDiv.append(larr)
-                           .append(rarr);
+                .append(rarr);
             $(_this).append(bg)
-                     .append(presentationDiv);
-            
-            $.each( data.photoset.photo, function(index) {
-                var url = "http://farm" + data.photoset.photo[index].farm + ".static.flickr.com/" + data.photoset.photo[index].server + "/" + data.photoset.photo[index].id + "_" + data.photoset.photo[index].secret + "_s.jpg"
+                .append(presentationDiv);
+
+            $.each(data.photoset.photo, function (index) {
+                var url = "http://farm" + data.photoset.photo[index].farm + ".static.flickr.com/" + data.photoset.photo[index].server + "/" + data.photoset.photo[index].id + "_" + data.photoset.photo[index].secret + "_s.jpg",
                     thumb_li = $("<li>"),
                     thumb_a = $("<a>").attr("href", url),
                     thumb = $("<img>").
                         attr("src", url);
                 thumbs_ul.append(thumb_li.append(thumb_a.append(thumb)));
-                thumb_a.click(function(event) {
+                thumb_a.click(function (event) {
                     event.preventDefault();
-                    var putPhoto = function(i) {
+                    var putPhoto = function (i) {
+                        var photo;
                         presentationDiv.children("img").remove();
                         presentationDiv.show().append(loading);
                         bg.show();
-                        if (i<=0) {larr.hide()} else {larr.show()};
-                        if (i>=data.photoset.photo.length-1) {rarr.hide()} else {rarr.show()};
+                        if (i <= 0) {larr.hide(); } else {larr.show(); }
+                        if (i >= data.photoset.photo.length - 1) {rarr.hide(); } else {rarr.show(); }
                         photo = $("<img>").
                             attr("src", "http://farm" + data.photoset.photo[i].farm + ".static.flickr.com/" + data.photoset.photo[i].server + "/" + data.photoset.photo[i].id + "_" + data.photoset.photo[i].secret + ".jpg").
                             hide().
-                            load(function(e) {
+                            load(function (e) {
                                 presentationDiv.append(photo);
-                                loading.animate({"line-height": $(photo).height()+"px"}, animationSpeed);
-                                $.map([larr,rarr], function(arr) {
+                                loading.animate({"line-height": $(photo).height() + "px"}, animationSpeed);
+                                $.map([larr, rarr], function (arr) {
                                     return arr.animate({
-                                        "width": (($(photo).width()/2)-20)+"px",
-                                        "height": ($(photo).height()-20)+"px",
-                                        "line-height": ($(photo).height()-20)+"px"
-                                    },animationSpeed).unbind('click');
+                                        "width": (($(photo).width() / 2) - 20) + "px",
+                                        "height": ($(photo).height() - 20) + "px",
+                                        "line-height": ($(photo).height() - 20) + "px"
+                                    }, animationSpeed).unbind('click');
                                 });
-                                larr.click(function() {
-                                    putPhoto(i-1);
+                                larr.click(function () {
+                                    putPhoto(i - 1);
                                 });
-                                rarr.click(function() {
-                                    putPhoto(i+1);
+                                rarr.click(function () {
+                                    putPhoto(i + 1);
                                 });
                                 presentationDiv.animate({
                                     "width": $(photo).width(),
                                     "height": $(photo).height(),
-                                    "margin-left": -(($(photo).width())/2),
-                                    "margin-top": -(($(photo).height())/2)
-                                }, animationSpeed, function() {
+                                    "margin-left": -(($(photo).width()) / 2),
+                                    "margin-top": -(($(photo).height()) / 2)
+                                }, animationSpeed, function () {
                                     loading.remove();
                                     photo.fadeIn();
                                 });
                             });
-                    }
+                    };
                     putPhoto(index);
                 });
             });
         };
-        
-        $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=" + apiKey + "&photoset_id=" + photosetId + "&format=json&jsoncallback=?", jsonCallback);	
 
-        
+        $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=" + apiKey + "&photoset_id=" + photosetId + "&format=json&jsoncallback=?", jsonCallback);
+
+
     };
 
-    $.fn[pluginName] = function ( options ) {
+    // A really lightweight plugin wrapper around the constructor, 
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function (options) {
         return this.each(function () {
             if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new FlickrSlidesPlugin( this, options ));
+                $.data(this, 'plugin_' + pluginName, new FlickrSlidesPlugin(this, options));
             }
         });
-    }
+    };
 
 })(jQuery, window);
